942f8af0df80f8d449427a7fc05b938857bb6217
landian60 <40920543+landian60@users.noreply.github.com>
2022-12-13 18:22:57 +0800

    change the error on stable.ipynb

--- ptp_utils.py
+++ ptp_utils.py
@@ -116,7 +116,8 @@ def text2image_ldm(
     latent, latents = init_latent(latent, model, height, width, generator, batch_size)
     context = torch.cat([uncond_embeddings, text_embeddings])
     
-    model.scheduler.set_timesteps(num_inference_steps)
+    model.scheduler.set_
+    steps(num_inference_steps)
     for t in tqdm(model.scheduler.timesteps):
         latents = diffusion_step(model, controller, latents, context, t, guidance_scale)
     
@@ -160,8 +161,9 @@ def text2image_ldm_stable(
     latent, latents = init_latent(latent, model, height, width, generator, batch_size)
     
     # set timesteps
-    extra_set_kwargs = {"offset": 1}
-    model.scheduler.set_timesteps(num_inference_steps, **extra_set_kwargs)
+#     extra_set_kwargs = {"offset": 1}
+#     model.scheduler.set_timesteps(num_inference_steps, **extra_set_kwargs)
+    model.scheduler.set_timesteps(num_inference_steps)
     for t in tqdm(model.scheduler.timesteps):
         latents = diffusion_step(model, controller, latents, context, t, guidance_scale, low_resource)
     
